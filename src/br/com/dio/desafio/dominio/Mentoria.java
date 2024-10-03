87dddb3f8ce082bada64ead3ee4ac263284283cd
muriloosantanaa <mukasantana1509@gmail.com>
2024-10-02 21:42:44 -0300

    subindo projeto

---
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,22 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+
+    public void setTitulo(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setTitulo'");
+    }
+
+    public void setDescricao(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setDescricao'");
+    }
+
+    public void setData(LocalDate now) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setData'");
+    }
+    
+}
